8034f710702153d78a2e94d4130a00bd5c30fbcb
rheven <rhus.mendoza.up@phinmaed.com>
2023-12-04 20:44:40 +0800

    okay

---
+++ app/src/main/java/com/mendoza/rhalfrheven/block3/p1/rhalfrhevenmendoza_block3_quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mendoza.rhalfrheven.block3.p1.rhalfrhevenmendoza_block3_quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
